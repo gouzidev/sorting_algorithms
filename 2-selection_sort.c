30892c9ebeb468ef75cc99745132eeb6ba8a97ff
gouzidev <gouzi.dev@gmail.com>
2023-07-22 23:17:41 +0100

    2O

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,37 +1,46 @@
 #include "sort.h"
+
+
 /**
- * selection_sort - sorts an array of integers in ascending
- * order using the Selection sort algorithm
+ * selection_sort - sorts
  *
- * @array: Array of integers
- * @size: Size of the array
- *
- * Return: Vois - No return
+ * @array: array
+ * @size: array size
  */
+
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
-	int tmp; /* Temporal variable */
+size_t i = 0;
+size_t j = 0;
+int min;
+size_t min_idx;
+int tmp;
 
-	if (array == NULL || size < 2)
-	{
-		return;
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		min_index = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_index])
-				min_index = j;
-		}
-		if (min_index != i)
-		{
-			tmp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = tmp;
-			print_array(array, size);
-		}
-	}
+if (array == NULL || size < 2)
+return;
+while (i < size)
+{
+min = array[i];
+j = i;
+min_idx = i;
+while (j < size)
+{
+if (min > array[j])
+{
+min = array[j];
+min_idx = j;
+}
+j++;
+}
+if (min_idx + 1 == i || min_idx == i)
+{
+i++;
+continue;
+}
+tmp = array[i];
+array[min_idx] = tmp;
+array[i] = min;
+i++;
+print_array(array, size);
+}
 }
