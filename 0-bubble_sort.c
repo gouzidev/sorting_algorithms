3229a3880eb31732ac653915910a6d3ffca4455d
gouzidev <gouzi.dev@gmail.com>
2023-07-22 21:44:32 +0100

    added task 0 files

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sorts array
+ * @array: list of integers
+ * @size: size of the list of integers
+ * Return: void
+*/
+void bubble_sort(int *array, size_t size)
+{
+size_t i = 0;
+size_t j = 0;
+int tmp;
+
+while (i < size)
+{
+j = 0;
+while (j < size - 1)
+{
+if (array[j] > array[j + 1])
+{
+tmp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = tmp;
+print_array(array, size);
+}
+j++;
+}
+i++;
+}
+}
