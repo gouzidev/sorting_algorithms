9fd3a25de1b3ff5e2f637e7500a614b8468ab55d
gouzidev <gouzi.dev@gmail.com>
2023-07-22 21:59:42 +0100

    check for null for task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,26 +8,26 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-size_t i = 0;
-size_t j = 0;
-int tmp;
+    size_t i = 0;
+    size_t j = 0;
+    int tmp;
 
-if (size < 2 || array == NULL)
-return;
-while (i < size)
-{
-j = 0;
-while (j < size - 1)
-{
-if (array[j] > array[j + 1])
-{
-tmp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = tmp;
-print_array(array, size);
-}
-j++;
-}
-i++;
-}
+    if (array == NULL || size < 2)
+    return;
+    while (i < size - 1)
+    {
+        j = 0;
+        while (j < size - 1)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+                print_array(array, size);
+            }
+            j++;
+        }
+        i++;
+    }
 }
