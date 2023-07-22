762ead9378d9a048d2f5bb071407e869a09e3aa6
gouzidev <gouzi.dev@gmail.com>
2023-07-22 22:02:35 +0100

    no idk why

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,40 +1,33 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending
- * order using the Bubble sort algorithm
- * @array: Array of integers
- * @size: Size of the array
- * Return: Void - No return
- */
+ * bubble_sort - bubble sorts array
+ * @array: list of integers
+ * @size: size of the list of integers
+ * Return: void
+*/
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int tmp; /* Temporal variable */
+size_t i = 0;
+size_t j = 0;
+int tmp;
 
-	if (array == NULL || size < 2)
-	{
-		return;
-	}
-
-	/* Travel the array */
-	for (i = 0; i < size - 1; i++)
-	{
-		/* loop for Compare */
-		for (j = 0; j < size - i - 1; j++)
-		{
-			/*
-			* if the number in the left is bigger than number
-			* in the right swap positions
-			*/
-			if (array[j] > array[j + 1])
-			{
-				/* Swap positions */
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				print_array(array, size);
-			}
-		}
-	}
+if (array == NULL || size < 2)
+return;
+while (i < size - 1)
+{
+j = 0;
+while (j < size - i - 1)
+{
+if (array[j] > array[j + 1])
+{
+tmp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = tmp;
+print_array(array, size);
+}
+j++;
+}
+i++;
+}
 }
