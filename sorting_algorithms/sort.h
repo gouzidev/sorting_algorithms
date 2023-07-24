95d2b0bef46fc63ef109449d38804b6184db45e6
chikhidev <chikhi.dev@gmail.com>
2023-07-24 12:28:47 +0100

    PUSH

--- sorting_algorithms/sort.h
+++ sorting_algorithms/sort.h
@@ -22,13 +22,19 @@ struct listint_s *next;
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
+/*helpers*/
+void swap_front(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_back(listint_t **list, listint_t **tail, listint_t **shaker);
+void insertion_sort_arr(int *arr, size_t size, size_t gap);
+
 /*mand:*/
 void bubble_sort(int *array, size_t size);
-void insertion_sort_arr(int *arr, size_t size, size_t gap);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
 /*advanced*/
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 #endif
